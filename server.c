56918d1d6bd87e526b5b174ab03e97e2704a2fb6
Ravid Dayan <dayanravid@gmail.com>
2023-07-27 02:51:13 +0300

    made a length and print function

--- server.c
+++ server.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+int boardLength(int board[3][3]){
+    return sizeof(board[0])/sizeof(board[0][0]);
+}
+
+void printBoard(int board[3][3]){
+    int length=boardLength(board);
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+        printf(" %d",board[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(){
-    printf("Hello");
+    int board[3][3]={{0,0,0},{0,0,0},{0,0,0}};
+    printBoard(board);
     return 0;
 }
