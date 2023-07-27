68a98b73525b637e713addf7bfd1308df7e317a6
Ravid Dayan <dayanravid@gmail.com>
2023-07-27 15:14:46 +0300

    add a find winner function edited print to print letters and not numbers

--- server.c
+++ server.c
@@ -9,13 +9,25 @@ void printBoard(int board[3][3]){
     int length=boardLength(board);
     for(int i=0;i<length;i++){
         for(int j=0;j<length;j++){
-            printf(" %d",board[i][j]);
+            switch (board[i][j])
+            {
+            case 0:
+                printf("__");
+                break;
+            case 1:
+                printf("X");
+                break;
+            
+            case 2:
+                printf("O");
+                break;
+            }
         }
         printf("\n");
     }
 }
 
-void getPlayerInput(int *board[3][3]){
+void getPlayerInput(int *pBoard[3][3]){
     int input;
     do{
         printf("please enter a number between 1 to 9 :");
@@ -31,8 +43,8 @@ void getPlayerInput(int *board[3][3]){
     }while(isCellLegal==0 || canBeInserted==0);
     return input;
 }
-int canBeInserted(int *board[3][3],int index){
-    return (*board[getRow(index)][getCOL(index)]==0)?1:0;
+int canBeInserted(int *pBoard[3][3],int index){
+    return (*pBoard[getRow(index)][getCOL(index)]==0)?1:0;
 }
 int isCellLegal(int index){
     return (index<1 || index>9)?1:0;
@@ -43,18 +55,39 @@ int getRow(int cell){
 int getCol(int cell){
         return (cell-1)%3;
 }
-void insertInput(int index,int sign,int *board[3][3]){
-    *board[getRow(index)][getCol(index)]=sign;
+void insertInput(int index,int sign,int *pBoard[3][3]){
+    *pBoard[getRow(index)][getCol(index)]=sign;
+}
+int findWinner(int board[3][3]){
+    for(int i=0;i<3;i++){
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][1]!=0){
+            return board[i][0];
+        }
+    }
+    for(int i=0;i<3;i++){
+        if(board[0][i]==board[1][i] && board[1][i]==board[2][i] && board[1][i]!=0){
+            return board[i][0];
+        }
+    }
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[1][1]!=0){
+        return board[1][1];
+    }
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[1][1]!=0){
+        return board[1][1];
+    }
+    return 0;
 }
 int main(){
     int board[3][3]={{0,0,0},{0,0,0},{0,0,0}};
+    int *pBoard[3][3]=&board;
     enum Signs{x=1,o=2};
     enum Signs playerSign;
-    bool winner=false;
+    int winner=0;
     int rounds=7;
-    /*while(winner && rounds==9){
-        rounds=rounds+1;
+    while(winner==0 || rounds<=9){
+        playerSign=x;
 
-    }*/
+
+    }
     return 0;
 }
