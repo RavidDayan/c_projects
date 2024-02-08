6720cf06facb955d24e4c0f9167a59132732a82e
Ravid Dayan <dayanravid@gmail.com>
2024-02-08 22:26:05 +0200

    made function add

--- matrixCalc/mymat.c
+++ matrixCalc/mymat.c
@@ -1,1 +1,48 @@
-#include <mymat.h>
+#include "mymat.h"
+
+void initializeMatrix(mat *result)
+{
+    int i;
+    int j;
+    for (i = 0; i < MAT_SIZE; i++)
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            result[i][j] = 0;
+        }
+    }
+}
+
+void add(mat *matA, mat *matB, mat *des)
+{
+    int i;
+    int j;
+    mat temp;/*temporary array to store result*/
+    initializeMatrix(temp);
+    for (i = 0; i < MAT_SIZE; i++)
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            temp[i][j] = matA[i][j] + matB[i][j];
+        }
+    }
+    for (i = 0; i < MAT_SIZE; i++)/*hard copy of result to destenation matrix*/
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            des[i][j] = temp[i][j];
+        }
+    }
+}
+void sub(mat *matA, mat *matB, mat *des)
+{
+}
+void mult(mat *matA, mat *matB, mat *des)
+{
+}
+void multS(mat *matA, float scalar, mat *des)
+{
+}
+void trans(mat *matA, mat *des)
+{
+}
--- matrixCalc/mymat.h
+++ matrixCalc/mymat.h
@@ -1,8 +1,10 @@
 
-typedef float mat [4][4];
+#define MAT_SIZE 4
+typedef float mat [MAT_SIZE][MAT_SIZE];
 
 void add(mat *matA,mat *matB,mat *des);
 void sub(mat *matA,mat *matB,mat *des);
 void mult(mat *matA,mat *matB,mat *des);
 void multS(mat *matA,float scalar,mat *des);
 void trans(mat *matA,mat *des);
+void initializeMatrix();
