bc7bc608fea98d1f22ddcb11a09d755926b18b19
Ravid Dayan <dayanravid@gmail.com>
2024-02-08 22:33:33 +0200

    made function sub

--- matrixCalc/mymat.c
+++ matrixCalc/mymat.c
@@ -36,6 +36,24 @@ void add(mat *matA, mat *matB, mat *des)
 }
 void sub(mat *matA, mat *matB, mat *des)
 {
+      int i;
+    int j;
+    mat temp;/*temporary array to store result*/
+    initializeMatrix(temp);
+    for (i = 0; i < MAT_SIZE; i++)
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            temp[i][j] = matA[i][j] - matB[i][j];
+        }
+    }
+    for (i = 0; i < MAT_SIZE; i++)/*hard copy of result to destenation matrix*/
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            des[i][j] = temp[i][j];
+        }
+    }  
 }
 void mult(mat *matA, mat *matB, mat *des)
 {
