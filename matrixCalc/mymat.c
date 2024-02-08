84bfc1c366f823dd77278fb705c5d6b0299cf643
Ravid Dayan <dayanravid@gmail.com>
2024-02-08 22:43:48 +0200

    made function mult

--- matrixCalc/mymat.c
+++ matrixCalc/mymat.c
@@ -17,7 +17,7 @@ void add(mat *matA, mat *matB, mat *des)
 {
     int i;
     int j;
-    mat temp;/*temporary array to store result*/
+    mat temp; /*temporary array to store result*/
     initializeMatrix(temp);
     for (i = 0; i < MAT_SIZE; i++)
     {
@@ -26,7 +26,7 @@ void add(mat *matA, mat *matB, mat *des)
             temp[i][j] = matA[i][j] + matB[i][j];
         }
     }
-    for (i = 0; i < MAT_SIZE; i++)/*hard copy of result to destenation matrix*/
+    for (i = 0; i < MAT_SIZE; i++) /*hard copy of result to destenation matrix*/
     {
         for (j = 0; j < MAT_SIZE; j++)
         {
@@ -36,9 +36,9 @@ void add(mat *matA, mat *matB, mat *des)
 }
 void sub(mat *matA, mat *matB, mat *des)
 {
-      int i;
+    int i;
     int j;
-    mat temp;/*temporary array to store result*/
+    mat temp; /*temporary array to store result*/
     initializeMatrix(temp);
     for (i = 0; i < MAT_SIZE; i++)
     {
@@ -47,16 +47,39 @@ void sub(mat *matA, mat *matB, mat *des)
             temp[i][j] = matA[i][j] - matB[i][j];
         }
     }
-    for (i = 0; i < MAT_SIZE; i++)/*hard copy of result to destenation matrix*/
+    for (i = 0; i < MAT_SIZE; i++) /*hard copy of result to destenation matrix*/
     {
         for (j = 0; j < MAT_SIZE; j++)
         {
             des[i][j] = temp[i][j];
         }
-    }  
+    }
 }
 void mult(mat *matA, mat *matB, mat *des)
 {
+    int i;
+    int j;
+    int k;
+    mat temp; /*temporary array to store result*/
+    initializeMatrix(temp);
+    for (i = 0; i < MAT_SIZE; i++)
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            for (k = 0; k < MAT_SIZE; k++)
+            {
+
+                temp[i][j] = matA[i][k] * matA[k][j];
+            }
+        }
+    }
+    for (i = 0; i < MAT_SIZE; i++) /*hard copy of result to destenation matrix*/
+    {
+        for (j = 0; j < MAT_SIZE; j++)
+        {
+            des[i][j] = temp[i][j];
+        }
+    }
 }
 void multS(mat *matA, float scalar, mat *des)
 {
