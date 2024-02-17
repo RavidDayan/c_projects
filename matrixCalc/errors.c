1a02b8c3ca2a69628e5136069b170b07a1742d43
Ravid Dayan <dayanravid@gmail.com>
2024-02-17 13:54:03 +0200

    finished all comments on error.h/c

--- matrixCalc/errors.c
+++ matrixCalc/errors.c
@@ -1,4 +1,6 @@
-#include "error.h"
+#include "errors.h"
+#include <stdio.h>
+
 int isError(int *args)
 {
     if (args[0] < 0)
@@ -9,4 +11,40 @@ int isError(int *args)
     {
         return 0;
     }
+}
+void notCommandError()
+{
+    printf("\nthe line is missing a legal command at the start\n");
+}
+void notMatrixError()
+{
+    printf("\nthe line is missing a legal matrix in it\n");
+}
+void missingCommaError()
+{
+    printf("\nthe line is missing a comma in it\n");
+}
+void notRealNumberError()
+{
+    printf("\nthis line has a non real number in it\n");
+}
+void extraCharactersError()
+{
+    printf("\nthis line has extra characters after the proper command\n");
+}
+void missingSpaceError()
+{
+    printf("\nthis line is missing a space after the command\n");
+}
+void ilegalCommaError()
+{
+    printf("\nthis line has an ilegal comma\n");
+}
+void consecutiveCommaError()
+{
+    printf("\nthis line has consecutive comma's\n");
+}
+void noScalarError()
+{
+    printf("\nthis line has no scalar\n");
 }
--- matrixCalc/errors.h
+++ matrixCalc/errors.h
@@ -1,12 +1,25 @@
 #ifndef ERRORS_H
 #define ERRORS_H
+/*each const defines a negative number to represent a specific error in the args interpetation */
 #define ERROR_NOT_COMMAND -1
 #define ERROR_NOT_MATRIX -2
-#define ERROR_MISSING_ARG -3
+#define ERROR_MISSING_COMMA -3
 #define ERROR_NOT_REAL_NUMBER -4
-#define ERROR_ILLEGAL_COMMA -5
-#define ERROR_ILLEGAL_EXTRA_CHARACTERS -6
-#define ERROR_MISSING_SPACE -7
+#define ERROR_ILLEGAL_EXTRA_CHARACTERS -5
+#define ERROR_MISSING_SPACE -6
+#define ERROR_ILLEGAL_COMMA -7
+#define ERROR_CONSECUTIVE_COMMA -8
+#define ERROR_NO_SCALAR -9
 
-int isError(int *args);
+int isError(int *args); /*recieves an arg representing int and return's if its an error or not (negtaive number is an error,postive command) */
+/*print function for each error available*/
+void notCommandError();
+void notMatrixError();
+void missingCommaError();
+void notRealNumberError();
+void extraCharactersError();
+void missingSpaceError();
+void ilegalCommaError();
+void consecutiveCommaError();
+void noScalarError();
 #endif
